36d08b33e91d02802a56e1c8655fc3f7154d3338
adrien allouche <aallouche@fortinet.com>
2023-08-30 17:50:26 +0300

    Adjust the card canvas Add and adjust Title and Word texts to the card canvas

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from tkinter import Tk, Canvas, PhotoImage, Button
 
 BACKGROUND_COLOR = "#B1DDC6"
-FONT_NAME = "Courier"
+FONT_NAME = "Arial"
 
 
 
@@ -19,8 +19,8 @@ canvas = Canvas(width=800, height=526, highlightthickness=0, bg=BACKGROUND_COLOR
 #  Creating the image to put in the canvas
 card_image = PhotoImage(file="images/card_front.png")
 canvas.create_image(410, 270, image=card_image)   # Placing the image approx at the center of the canvas
-title = canvas.create_text(390, 150, text="Title", fill="black", font=(FONT_NAME, 20, "italic", "bold"))    # Place the title in the upper center of the card
-word_to_convert = canvas.create_text(390, 260, text="Word", fill="black", font=(FONT_NAME, 30, "bold"))
+title = canvas.create_text(400, 150, text="Title", fill="black", font=(FONT_NAME, 40, "italic"))    # Place the title in the upper center of the card
+word_to_convert = canvas.create_text(400, 263, text="Word", fill="black", font=(FONT_NAME, 60, "bold"))
 canvas.grid(column=0, row=0, columnspan=2)
 
 # Create buttons
